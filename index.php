ec4f697cc56241526161ed41c935b6f6af810197
patrick <patrick204nqh@gmail.com>
2020-06-01 13:59:04 +0700

    variables & contants

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-  // echo 'hello, Patrick';
+
+  define('NAME', 'Patrick');
+
+  // $name = "Patrick";
+  $age = 20;
+
+  // $name = 'Robert';
+
 ?>
 
 <!DOCTYPE html>
@@ -7,9 +14,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My first PHP file</title>
+  <title>PHP</title>
 </head>
 <body>
-  <h1><?php echo 'hello, Patrick'; ?></h1>
+  <h1>User Profile Page</h1>
+
+  <div><?php echo NAME; ?></div>
+  <div><?php echo $age; ?></div>
 </body>
 </html>
