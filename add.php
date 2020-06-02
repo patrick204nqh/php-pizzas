80d16b018721c39f94dfd690dcd075491f93d321
patrick <patrick204nqh@gmail.com>
2020-06-03 00:43:50 +0700

    validation with filters

--- add.php
+++ add.php
@@ -15,21 +15,30 @@ if (isset($_POST['submit'])) {
   if (empty($_POST['email'])) {
     echo 'An email is required <br />';
   } else {
-    echo htmlspecialchars($_POST['email']);
+    $email = $_POST['email'];
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo 'email must be a valid email address';
+    }
   }
 
   // check title
   if (empty($_POST['title'])) {
     echo 'An title is required <br />';
   } else {
-    echo htmlspecialchars($_POST['title']);
+    $title = $_POST['title'];
+    if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+      echo 'Title must be letters and spaces only';
+    }
   }
 
   // check ingredients
   if (empty($_POST['ingredients'])) {
     echo 'At least one ingredient is required <br />';
   } else {
-    echo htmlspecialchars($_POST['ingredients']);
+    $ingredients = $_POST['ingredients'];
+    if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+      echo 'Ingredients must be a comma separated list';
+    }
   }
 } // end of POSt check
 
