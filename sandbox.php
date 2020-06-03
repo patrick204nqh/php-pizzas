759ef7d019eab707cb0c63c989fb1ff46b408db6
patrick <patrick204nqh@gmail.com>
2020-06-03 17:40:33 +0700

    ternary operators

---
+++ sandbox.php
@@ -0,0 +1,29 @@
+<?php
+
+// ternary operators
+
+$score = 50;
+
+// if($score > 40) {
+//   echo 'high score';
+// } else {
+//   echo 'low score';
+// }
+
+// echo $score > 40 ? 'high score' : 'low score';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Document</title>
+</head>
+
+<body>
+  <p><?php echo $score > 40 ? 'high score' : 'low score'; ?></p>
+
+</body>
+
+</html>
