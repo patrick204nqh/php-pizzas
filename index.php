bdce2883e2baa6a2ca81c2d31fbf6b395393d24f
patrick <patrick204nqh@gmail.com>
2020-06-03 14:12:31 +0700

    getting data from database

--- index.php
+++ index.php
@@ -8,6 +8,23 @@ if (!$conn) {
   echo 'Connection error: ' . mysqli_connect();
 }
 
+// write query for all pizzas
+$spl = 'SELECT title, ingredients, id FROM pizzas';
+
+// make query & get result
+$result = mysqli_query($conn, $spl);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+print_r($pizzas);
+
 ?>
 
 <!DOCTYPE html>
