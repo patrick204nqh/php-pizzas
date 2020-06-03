cc29d2cecccabc85131b51cc128c4df966640c8f
patrick <patrick204nqh@gmail.com>
2020-06-03 14:55:14 +0700

    rendering data

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (!$conn) {
 }
 
 // write query for all pizzas
-$spl = 'SELECT title, ingredients, id FROM pizzas';
+$spl = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 // make query & get result
 $result = mysqli_query($conn, $spl);
@@ -23,14 +23,32 @@ mysqli_free_result($result);
 // close connection
 mysqli_close($conn);
 
-print_r($pizzas);
-
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <?php include('templates/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+  <div class="row">
+    <?php foreach ($pizzas as $pizza) { ?>
+      <div class="col s6 md3">
+        <div class="card z-depth-0">
+          <div class="card-content center">
+            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+          </div>
+          <div class="card-action right-align">
+            <a href="#" class="brand-text">more info</a>
+          </div>
+        </div>
+      </div>
+    <?php } ?>
+  </div>
+</div>
 
 <?php include('templates/footer.php'); ?>
 
