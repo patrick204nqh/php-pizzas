e782f85cc8889145f82f6cf94d7c41488aeb26c5
patrick <patrick204nqh@gmail.com>
2020-06-03 17:33:45 +0700

    finishing touches

--- details.php
+++ details.php
@@ -42,7 +42,7 @@ if (isset($_GET['id'])) {
 
 <?php include('templates/header.php'); ?>
 
-<div class="container center">
+<div class="container center grey-text">
   <?php if ($pizza) : ?>
     <h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
     <p>Create by: <?php htmlspecialchars($pizza['email']); ?></p>
